d559947378c1f888e528a2f2230e0b174d33c076
Anthony Suárez <neolight1010@gmail.com>
2022-11-09 22:36:55 -0500

    Configure pre-commit and format all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'hia-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "hia-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'hotelinventoryapp';
+  title = "hotelinventoryapp";
 }
