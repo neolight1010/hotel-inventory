81f2a378b1fd47bad68b997bbff3fab34eef19f1
Anthony Suárez <neolight1010@gmail.com>
2022-11-14 19:39:56 -0500

    Implement initial rooms component

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Room, Rooms } from "./rooms";
 
 @Component({
   selector: "hia-rooms",
@@ -8,6 +9,42 @@ import { Component, OnInit } from "@angular/core";
 export class RoomsComponent implements OnInit {
   hotelName = "Neo Hotel";
   numberOfRooms = 10;
+
+  rooms: Rooms = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5,
+  };
+
+  roomList: Room[] = [
+    {
+      roomNumber: 1,
+      roomType: "Deluxe Room",
+      amenities: "Air Conditioner, Free Wi-Fi, TV, Kitchen",
+      price: 500,
+      photo: "",
+      checkInTime: new Date("11/10/2022"),
+      checkOutTime: new Date("11/14/2022"),
+    },
+    {
+      roomNumber: 2,
+      roomType: "Regular Room",
+      amenities: "TV",
+      price: 200,
+      photo: "",
+      checkInTime: new Date("11/9/2022"),
+      checkOutTime: new Date("11/11/2022"),
+    },
+    {
+      roomNumber: 3,
+      roomType: "Cheap room",
+      amenities: "Nothing",
+      price: 100,
+      photo: "",
+      checkInTime: new Date("11/8/2022"),
+      checkOutTime: new Date("11/13/2022"),
+    },
+  ];
 
   hideRooms = false;
 
