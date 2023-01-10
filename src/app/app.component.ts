4ff426155cb186438c2baa3faa14dcb6150b22e0
Anthony Suárez <neolight1010@gmail.com>
2023-01-09 21:23:07 -0500

    Create logger service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, Optional } from "@angular/core";
+import { LoggerService } from "./logger.service";
 
 @Component({
   selector: "hia-root",
@@ -7,4 +8,8 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "hotelinventoryapp";
+
+  constructor(@Optional() private loggerService?: LoggerService) {
+    this.loggerService?.log("Hello World");
+  }
 }
