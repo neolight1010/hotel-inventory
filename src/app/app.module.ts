a0f6f9d91d201a60e951b4115d7c69dab534949a
Anthony Suárez <neolight1010@gmail.com>
2023-01-11 22:14:05 -0500

    Import HttpClientModule in app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
@@ -20,7 +21,12 @@ import { APP_CONFIG, AppConfigService } from "./app-config/app-config.service";
     ContainerComponent,
     EmployeeComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+  ],
   providers: [
     {
       provide: AppConfigService,
