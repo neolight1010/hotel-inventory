29d910ba5941cc5ad5bfb23635544dce1db4049e
Anthony Suárez <neolight1010@gmail.com>
2023-02-09 22:12:20 -0500

    Catch and display value in RoomBooking component

--- src/app/room-booking/room-booking.component.ts
+++ src/app/room-booking/room-booking.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { map, Observable } from "rxjs";
 
 @Component({
-  selector: 'hia-room-booking',
-  templateUrl: './room-booking.component.html',
-  styleUrls: ['./room-booking.component.scss']
+  selector: "hia-room-booking",
+  templateUrl: "./room-booking.component.html",
+  styleUrls: ["./room-booking.component.scss"],
 })
 export class RoomBookingComponent implements OnInit {
+  roomId$: Observable<number> = this.router.params.pipe(
+    map((params) => params["id"])
+  );
 
-  constructor() { }
+  constructor(private router: ActivatedRoute) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
