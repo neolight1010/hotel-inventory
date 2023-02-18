d854983c6771d38da41a632247470a6cd245da8b
Anthony Suárez <neolight1010@gmail.com>
2023-02-17 19:44:21 -0500

    Navigate on login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'hia-login',
@@ -9,14 +10,14 @@ export class LoginComponent implements OnInit {
   email: string = "";
   password: string = "";
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
   login(): void {
     if (this.email === "admin" && this.password === "admin") {
-      alert("Login Successful");
+      this.router.navigateByUrl("/rooms/add");
     } else {
       alert(`Invalid Credentials ${this.email} ${this.password}`);
     }
