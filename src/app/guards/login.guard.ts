45572c79da32fc406d7811671fd0062969340c3c
Anthony Suárez <neolight1010@gmail.com>
2023-02-22 21:27:13 -0500

    Use LoginService in LoginGuard

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import {LoginService} from '../login/login.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginGuard implements CanActivate, CanLoad {
+  constructor (private loginService: LoginService) {
+  }
+
   canActivate(
     _route: ActivatedRouteSnapshot,
     _state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.loginService.isLoggedIn;
   }
   canLoad(
     _route: Route,
