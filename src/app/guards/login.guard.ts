aa90b48ba71207764d4bf2db2789244b78ae1bfd
Anthony Suárez <neolight1010@gmail.com>
2023-02-22 21:29:11 -0500

    Navigate to login page on failed authentication

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,23 +1,41 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
-import {LoginService} from '../login/login.service';
+import { Injectable } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree,
+} from "@angular/router";
+import { Observable } from "rxjs";
+import { LoginService } from "../login/login.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class LoginGuard implements CanActivate, CanLoad {
-  constructor (private loginService: LoginService) {
-  }
+  constructor(private loginService: LoginService, private router: Router) {}
 
   canActivate(
     _route: ActivatedRouteSnapshot,
-    _state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.loginService.isLoggedIn;
+    _state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.loginService.isLoggedIn || this.router.navigate(["/login"]);
   }
   canLoad(
     _route: Route,
-    _segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    _segments: UrlSegment[]
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
     return true;
   }
 }
