da82210f5d1cb6871bd2796ad56251b66a7a8d25
Anthony Suárez <neolight1010@gmail.com>
2023-02-23 17:18:47 -0500

    Allow accessing Room routes only if admin

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -5,11 +5,15 @@ import { Injectable } from "@angular/core";
 })
 export class LoginService {
   isLoggedIn = false;
+  isAdmin = false;
 
   constructor() {}
 
   login(email: string, password: string): boolean {
     if (email === "admin" && password === "admin") {
+      this.isLoggedIn = true;
+      this.isAdmin = true;
+    } else if (email === "user" && password === "user") {
       this.isLoggedIn = true;
     }
 
--- src/app/rooms/guards/room.guard.ts
+++ src/app/rooms/guards/room.guard.ts
@@ -1,15 +1,27 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateChild, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  CanActivateChild,
+  RouterStateSnapshot,
+  UrlTree,
+} from "@angular/router";
+import { Observable } from "rxjs";
+import { LoginService } from "../../login/login.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class RoomGuard implements CanActivateChild {
+  constructor(private loginService: LoginService) {}
+
   canActivateChild(
     _childRoute: ActivatedRouteSnapshot,
-    _state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return false;
+    _state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.loginService.isAdmin;
   }
-
 }
