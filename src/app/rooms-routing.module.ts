17fe6b85b3e2ac8cdc38876e760d0819217b8a71
Anthony Suárez <neolight1010@gmail.com>
2023-02-23 17:16:25 -0500

    Create empty RoomGuard

--- src/app/rooms-routing.module.ts
+++ src/app/rooms-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { RoomAddComponent } from "./room-add/room-add.component";
 import { RoomBookingComponent } from "./room-booking/room-booking.component";
+import {RoomGuard} from "./rooms/guards/room.guard";
 import { RoomsComponent } from "./rooms/rooms.component";
 
 const routes: Routes = [
   {
     path: "",
     component: RoomsComponent,
+    canActivateChild: [RoomGuard],
     children: [
       { path: "add", component: RoomAddComponent },
       { path: ":id", component: RoomBookingComponent },
