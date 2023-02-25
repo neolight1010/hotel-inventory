62360f20b3e40bae724e8ff77bcc48bbb6b54cc2
Anthony Suárez <neolight1010@gmail.com>
2023-02-24 20:30:57 -0500

    Remove login guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { EmployeeComponent } from "./employee/employee.component";
-import { LoginGuard } from "./guards/login.guard";
 import { LoginComponent } from "./login/login.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 
@@ -9,15 +8,12 @@ const routes: Routes = [
   {
     path: "employee",
     component: EmployeeComponent,
-    canActivate: [LoginGuard],
   },
 
   { path: "login", component: LoginComponent },
   {
     path: "rooms",
     loadChildren: () => import("./rooms.module").then((m) => m.RoomsModule),
-    canActivate: [LoginGuard],
-    canLoad: [LoginGuard],
   },
 
   { path: "", redirectTo: "/rooms", pathMatch: "full" },
@@ -25,7 +21,6 @@ const routes: Routes = [
     path: "booking",
     loadChildren: () =>
       import("./booking/booking.module").then((m) => m.BookingModule),
-    canActivate: [LoginGuard],
   },
   { path: "**", component: NotFoundComponent },
 ];
