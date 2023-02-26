9aa50c3243707c24e90b48e1d21dbbbf2c7823a9
Anthony Suárez <neolight1010@gmail.com>
2023-02-26 12:22:28 -0500

    Create empty booking form

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'hia-booking',
-  templateUrl: './booking.component.html',
-  styleUrls: ['./booking.component.scss']
+  selector: "hia-booking",
+  templateUrl: "./booking.component.html",
+  styleUrls: ["./booking.component.scss"],
 })
 export class BookingComponent implements OnInit {
+  bookingForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-  }
+    const initialBooking: Booking = {
+      roomId: "",
+      guestEmail: "",
+      checkInDate: new Date(),
+      checkOutDate: new Date(),
+      bookingStatus: "",
+      bookingAmount: 0,
+      bookingDate: new Date(),
+      mobileNumber: "",
+      guestName: "",
+      guestAddress: "",
+      guestCity: "",
+      guestState: "",
+      guestCountry: "",
+      guestZipCode: "",
+      guestCount: 0,
+      guestList: [],
+    };
 
+    this.bookingForm = this.formBuilder.group(initialBooking);
+  }
 }
+
+interface Booking {
+  roomId: string;
+  guestEmail: string;
+  checkInDate: Date;
+  checkOutDate: Date;
+  bookingStatus: string;
+  bookingAmount: number;
+  bookingDate: Date;
+  mobileNumber: string;
+  guestName: string;
+  guestAddress: string;
+  guestCity: string;
+  guestState: string;
+  guestCountry: string;
+  guestZipCode: string;
+  guestCount: number;
+  guestList: Guest[];
+}
+
+interface Guest {}
--- src/app/booking/booking.module.ts
+++ src/app/booking/booking.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { BookingRoutingModule } from './booking-routing.module';
-import { BookingComponent } from './booking.component';
-
+import { BookingRoutingModule } from "./booking-routing.module";
+import { BookingComponent } from "./booking.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
-  declarations: [
-    BookingComponent
-  ],
+  declarations: [BookingComponent],
   imports: [
     CommonModule,
-    BookingRoutingModule
-  ]
+    BookingRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
 })
-export class BookingModule { }
+export class BookingModule {}
